026be156d6b805233c9daabf8a0d85b8eec058d2
Sajjad <mohammadsajjadasaadi@gmail.com>
2018-11-06 22:06:54 +0330

    add 'خرید اینترنتی' , 'برداشت' , 'خودپرداز' to SMS Parser

--- app/src/main/java/com/example/sejil/myapplication/SMSreceiver.java
+++ app/src/main/java/com/example/sejil/myapplication/SMSreceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
 
 public class SMSreceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
@@ -26,13 +27,25 @@ public class SMSreceiver extends BroadcastReceiver {
             }
 
             if (msgsender != null) {
-                if (msgsender.equals("+9820004000") || msgsender.equals("Bank Mellat")) {
-                    Intent i = new Intent(context, MainActivity.class);
-                    i.putExtra("MSG_SENDER", msgsender);
-                    i.putExtra("MSG_BODY", msgbody);
+                if (msgsender.equals("+9820004000")) {
+                    String[] split_enters = msgbody.split("\n");
+                    String[] split_money = split_enters[1].split(":");
+                    if (split_money[0].equals("انتقال") || split_money[0].equals("خریداینترنتی") || split_money[0].equals("برداشت") || split_money[0].equals("خودپرداز")) {
+                        Intent i = new Intent(context, MainActivity.class);
+                        i.putExtra("MSG_SENDER", msgsender);
+                        i.putExtra("MSG_BODY", msgbody);
 
-                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    context.startActivity(i);
+                        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        context.startActivity(i);
+                    }
+                }else if(msgsender.equals("Bank Mellat")){
+                    Log.d("onReceive", "onReceive: BANK MELLAT");
+//                    Intent i = new Intent(context, MainActivity.class);
+//                    i.putExtra("MSG_SENDER", msgsender);
+//                    i.putExtra("MSG_BODY", msgbody);
+//
+//                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                    context.startActivity(i);
                 }
             }
         }
