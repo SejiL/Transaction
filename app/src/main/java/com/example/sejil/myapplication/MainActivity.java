1a54e0ace5da54cf79ee4d90705d22e44176d49f
MohammadReza Salari <frishter.ms@gmail.com>
2019-04-28 13:33:09 +0430

    create BaseActivity class.

--- app/src/main/java/com/example/sejil/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sejil/myapplication/MainActivity.java
@@ -4,9 +4,10 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
+
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,17 +16,22 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.sejil.myapplication.Base.BaseActivity;
+
 import java.util.Calendar;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int getContentViewRes() {
+        return R.layout.activity_main;
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         final ImageView imageMain = findViewById(R.id.iv_main);
         final TextView mainText = findViewById(R.id.tv_main);
         final Button btnSave = findViewById(R.id.save_bt);
@@ -101,7 +107,6 @@ public class MainActivity extends AppCompatActivity {
 //                }
 //            }
 //        });
-
     }
 
     private void askForPermission(String permission, Integer requestCode) {
