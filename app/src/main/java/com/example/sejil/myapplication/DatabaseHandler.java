4470294ff180811225d4addacc45fe44b312fed4
MohammadReza Salari <frishter.ms@gmail.com>
2019-04-28 12:31:02 +0430

    Migrate to AndroidX. Remove test implementations.

--- app/src/main/java/com/example/sejil/myapplication/DatabaseHandler.java
+++ app/src/main/java/com/example/sejil/myapplication/DatabaseHandler.java
@@ -5,8 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.support.annotation.Nullable;
-import android.util.Log;
+import androidx.annotation.Nullable;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
